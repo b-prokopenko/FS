524ee5bd3b656b91cb9df288ac0da1f8333c01f8
Bohdan Prokopenko <bohdanpr@interlink-ua.com>
2019-09-28 10:52:54 +0300

    Renamed metod

--- FSCoreLibrary/FSCore.cs
+++ FSCoreLibrary/FSCore.cs
@@ -6,10 +6,10 @@ namespace FSCoreLibrary
 {
     public static class FSCore
     {
-        public static IPropertyService PropertyService { get => GetService<PropertyService>(); }
-        public static IPathService PathService { get => GetService<PathService>(); }
+        public static IPropertyService PropertyService { get => GetInstanceOf<PropertyService>(); }
+        public static IPathService PathService { get => GetInstanceOf<PathService>(); }
 
-        private static T GetService<T>()
+        private static T GetInstanceOf<T>()
         {
             return (T)Activator.CreateInstance(typeof(T));
         }
