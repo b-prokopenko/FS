2d37e443571723c99bb2fa6da7dd662d752448ff
Bohdan Prokopenko <bohdanpr@interlink-ua.com>
2019-10-01 22:42:15 +0300

    Initialized public IFSForm interface Initialized TestData class for the test classes

---
+++ FSGuiTests/Data/TestData.cs
@@ -0,0 +1,14 @@
+﻿using FSWFGui.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSGuiTests.Data
+{
+    static class TestData
+    {
+        public static IFSForm MainForm { get; set; }
+    }
+}
---
+++ FSGuiTests/FSWFGuiTests/IFSFormTest.cs
@@ -0,0 +1,16 @@
+﻿using FSGuiTests.Data;
+using FSWFGui.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FSGuiTests.FSWFGuiTests
+{
+    [TestClass]
+    public class IFSFormTest
+    {
+        IFSForm TestForm = TestData.MainForm;
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
