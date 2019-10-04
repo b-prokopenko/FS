f7e44a356ecaaa69a1f011243d8a61b5cc2649cf
Bohdan Prokopenko <bohdanpr@interlink-ua.com>
2019-10-04 19:45:21 +0300

    Fixed test

--- FSCoreLibraryTests/Integration/FSCoreTest.cs
+++ FSCoreLibraryTests/Integration/FSCoreTest.cs
@@ -9,15 +9,16 @@ namespace FSCoreLibraryTests.Integration
     public class FSCoreTest
     {
         ISortParams Params = DataService.Data.SortParams;
+        ISortService Service = FSCore.Service;
 
         [Fact]
         void ShouldSortFiles()
         {
             DataService.Data.PrepareFolder(Params.SourceFolder);
             DataService.Data.PrepareFiles(20, 5, Params.SourceFolder);
-            FSCore.Service.Sort(Params);
-            var actualTotal = FSCore.Service.Total;
-            var actualReady = FSCore.Service.Ready;
+            Service.Sort(Params);
+            var actualTotal = Service.Total;
+            var actualReady = Service.Ready;
             var files = Directory.GetFiles(Params.TargetFolder, "*", SearchOption.AllDirectories);
             Assert.Equal(files.Length, actualTotal);
             Assert.Equal(files.Length, actualReady);
